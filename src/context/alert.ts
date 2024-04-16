b4de8c40198d12db60b401fac9dbed0362cf6ce8
Maximviktorovic2305 <Maximviktorovic123@ya.ru>
2024-04-16 19:55:15 +1000

    git cache cleared

--- src/context/alert.ts
+++
@@ -1,8 +0,0 @@
-import { createDomain } from 'effector';
-import { IAlert } from '../types/index';
-const error = createDomain();
-
-export const setAlert = error.createEvent<IAlert>()
-
-export const $alert = error.createStore<IAlert>({ alertText: '', alertStatus: '' })
-    .on(setAlert, (_, value) => value)
